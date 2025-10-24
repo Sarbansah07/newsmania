b42fab7cbd7c165f6aa8002d1310284b0554c201
Sarbansah07 <sahsarban76@gmail.com>
2025-10-24 12:09:29 +0530

    first commit

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = 'news-app-cache-v1';
+const CACHE_NAME = 'news-app-cache-v2';
 const urlsToCache = [
     './',
     './index.html',
@@ -9,9 +9,24 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting(); // Force the new service worker to activate immediately
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim()) // Take control of all clients immediately
     );
 });
 
