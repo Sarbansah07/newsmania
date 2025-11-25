3212eac39acdbe3b9d1cd2f67d3411afe4b80a0d
Sarbansah07 <150502886+Sarbansah07@users.noreply.github.com>
2025-11-25 18:27:27 +0530

    Change server host to '0.0.0.0'

--- server.js
+++ server.js
@@ -170,7 +170,7 @@ app.post('/api/summarize', async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-const HOST = 'localhost';
+const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
